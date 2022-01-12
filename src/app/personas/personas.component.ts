0021fe9834081c1ca2442059ea5404fa0288b3f8
hectorvcode <hectorfabiovasquez@gmail.com>
2022-01-12 17:21:38 -0500

    Generated inline components

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,10 +1,12 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-personas',
-    templateUrl: './personas.component.html',
-    
+  selector: 'app-personas',
+  template: `
+    <h1>Listado de personas</h1>
+    <app-persona></app-persona>
+    <app-persona></app-persona>
+  `,
+  /* templateUrl: './personas.component.html', */
 })
-export class PersonasComponent {
-    
-}
+export class PersonasComponent {}
