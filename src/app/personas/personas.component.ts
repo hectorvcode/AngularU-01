c9e504df6e396d77b881f9143c548deb5421d9b4
hectorvcode <hectorfabiovasquez@gmail.com>
2022-01-12 17:59:08 -0500

    Applied CSS styles to Angular

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,12 +1,19 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
-  template: `
+  /* Para agregar codigo inline, no creamos template en archivo externo 
+    template: `
     <h1>Listado de personas</h1>
     <app-persona></app-persona>
     <app-persona></app-persona>
-  `,
-  /* templateUrl: './personas.component.html', */
+  `, */
+  templateUrl: './personas.component.html',
+  styles:[`
+    h1{
+      color:blue;
+    }
+  `]
+  //styleUrls: ['./personas.component.css']
 })
 export class PersonasComponent {}
