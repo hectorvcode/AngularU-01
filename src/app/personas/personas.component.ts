ad932d7bbe6fb772f3b1a3447be7987c2b33f9c0
hectorvcode <hectorfabiovasquez@gmail.com>
2022-01-28 22:18:23 -0500

    Added Event Binding part 1

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-personas',
   /* Para agregar codigo inline, no creamos template en archivo externo 
-    template: `
-    <h1>Listado de personas</h1>
-    <app-persona></app-persona>
-    <app-persona></app-persona>
+  template: `
+  <h1>Listado de personas</h1>
+  <app-persona></app-persona>
+  <app-persona></app-persona>
   `, */
+  selector: 'app-personas',
   templateUrl: './personas.component.html',
-  styles:[`
+  styleUrls: ['./personas.component.css']
+  /* styles:[`
     h1{
       color:blue;
     }
-  `]
-  //styleUrls: ['./personas.component.css']
+  `] */
 })
 export class PersonasComponent {
   deshabilitar = false;
+  mensaje = 'No se ha agregado ninguna persona'
+
+  agregarPersona(){
+    this.mensaje = 'Persona agregada';
+  }
 }
